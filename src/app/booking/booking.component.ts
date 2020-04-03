d02d2643df4f760a7eaea3a3eef4c9e8538a30b0
Aleem Pasha <aleem.pasha@jdplc.com>
2020-04-03 13:03:09 +0530

    Dates dynamic functionality implemente

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-booking',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking.component.css']
 })
 export class BookingComponent implements OnInit {
+  public selectedDate = moment()
+    .startOf("day")
+    .format();
+  public dates = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this._changeDisplayedWeek();
   }
 
+  prevWeek() {
+    this._changeDisplayedWeek(-7);
+  }
+
+  nextWeek = function() {
+    this._changeDisplayedWeek(7);
+  };
+
+  _changeDisplayedWeek(daysToAdd = 0) {
+    var selectedDate = moment(this.selectedDate).add(daysToAdd, "days");
+    this.selectedDate = selectedDate.format();
+    this.dates = this._expandWeek(this.selectedDate);
+  }
+
+  _expandWeek(startDate) {
+    var dates = [];
+    var dayOfWeek = moment(startDate);
+    for (var i = 0; i < 7; i++) {
+      dates.push({
+        weekDay: dayOfWeek.format("ddd").toUpperCase(),
+        shortDate: dayOfWeek.format("DD MMM").toUpperCase(),
+        year: dayOfWeek.format("YYYY"),
+        date: dayOfWeek.format(),
+        slots: 3 // here we need to get the count of avaialable slots
+      });
+      dayOfWeek.add(1, "d");
+    }
+    return dates;
+  }
 }
